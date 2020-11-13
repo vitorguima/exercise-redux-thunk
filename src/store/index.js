888be6755a7bd6c4a07958d860a73cf0140232f3
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2020-11-13 12:41:59 -0300

    setting up exercisa-one

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 // Importe o método applyMiddleware
-import { applyMiddleware, createStore } from 'redux';
+import { createStore } from 'redux';
 // Importe o redux-thunk
-import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 // Aplique o middleware
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
 );
+
 
 export default store;
